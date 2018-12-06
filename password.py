bdc78047af2f7f86593a4059996c78a30734bb85
Sam Ou <lll810502@gmail.com>
2018-12-06 15:04:16 +0800

    更新版無0

--- password.py
+++ password.py
@@ -2,7 +2,7 @@
 
 time = 1
 
-while time<=3 :
+while time <= 3 :
 
     password = input('請輸入密碼(最多三次)')
     
@@ -11,8 +11,11 @@ while time<=3 :
         print('密碼正確，請進入')
         break
     elif password != 'a123456' :
-        print('密碼錯誤！你還有', 3 - time, '次機會')
+        print('密碼錯誤！')
+        if time <= 2:
+            print('你還有', 3 - time, '次機會')
         time = time + 1 
     if time == 4 :
-                print('達次數上限，已鎖定程式')
+        print('達次數上限，已鎖定程式')
+        break
     
