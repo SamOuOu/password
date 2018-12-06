38699034107ea0adf91ee7a6d1cd5c1c6e4e65d1
Sam Ou <lll810502@gmail.com>
2018-12-06 14:42:30 +0800

    初步成功版

---
+++ password.py
@@ -0,0 +1,18 @@
+#密碼重複輸入程式，最多三次
+
+time = 1
+
+while time<=3 :
+
+    password = input('請輸入密碼(最多三次)')
+    
+
+    if password == 'a123456':
+        print('密碼正確，請進入')
+        break
+    elif password != 'a123456' :
+        print('密碼錯誤！你還有', 3 - time, '次機會')
+        time = time + 1 
+    if time == 4 :
+                print('達次數上限，已鎖定程式')
+    
